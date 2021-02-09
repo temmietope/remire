8ccbb6909c4b1d2a8fd1aea5e79db9645e43cb2c
temmietope <temmieayodele@gmail.com>
2021-02-09 16:21:31 +0100

    added github link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,8 @@ import * as ROUTES from '../routing/routes'
 import { Nav, NavbarWrapper } from '../theme/styles'
 
 import { ReactComponent as BrandIcon } from '../assets/icons/starwars.svg'
+import { ReactComponent as GithubIcon } from '../assets/icons/github.svg'
+
 import { Link } from 'react-router-dom'
 import React from 'react'
 
@@ -12,6 +14,13 @@ const Navbar = () => (
       <Link to={ROUTES.HOME}>
         <BrandIcon />
       </Link>
+      <a
+        href="https://github.com/temmietope/star-wars"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <GithubIcon className="github" />
+      </a>
     </Nav>
   </NavbarWrapper>
 )
